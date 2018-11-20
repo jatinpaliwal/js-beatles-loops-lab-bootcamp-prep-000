e59ff43c7e78cfe409ea20da1aecc5cc758336b3
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-20 09:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments)
 {
-  var arr=[];
-  for (var i=0;i<musicians.length;i++) 
+  var arr=[]
+  for (var i=0;i<musicians.length;i++)
   {
     arr[i]='${musicians[i]} plays ${instruments[i]}'
   }
+  return arr
 }
