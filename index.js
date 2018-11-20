0de76a53be637c7b19786305357f405c2e57098a
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-20 09:13:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments)
+{
+  var arr=[];
+  for (var i=0;i<musicians.length;i++) 
+  {
+    arr[i]='${musicians[i]} plays ${instruments[i]}'
+  }
+}
