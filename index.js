c2cc97cefc9bb7459146511b00791f6dd18bc087
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-20 09:30:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 function theBeatlesPlay(musicians,instruments)
 {
   var arr=[]
+  var str
   for (var i=0;i<musicians.length;i++)
   {
-    arr[i]='${musicians[i]} plays ${instruments[i]}'
+    str=musicians[i] +'plays'+ instruments[i];
+    arr[i]=str
   }
   return arr
+}
+
+function johnLennonFacts(facts)
+{
+  var i=0
+  while (facts.length>i) 
+  {
+    
+  }
 }
